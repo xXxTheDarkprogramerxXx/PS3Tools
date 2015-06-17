dae2766ded81584c1c72cd1d074383432f9e9df7
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2015-06-17 18:01:53 +0200

    PS3 ISO TOOL
    PS3 Tool GUI Added

---
+++ PS3 ISO Tool GUI/PS3 ISO Tools GUI/ps3_ISO_Tool_GUI.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PS3_ISO_Tools_GUI
+{
+    partial class ps3_ISO_Tool_GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PS3_ISO_Tools_GUI.Properties.Resources.playstation_3_logo_pngmadden_nfl_25_cheats__trytoolzcom___cheats_bot_trainer_cheat_zwd9zhkq;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ISO TOOL GUI";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.ForeColor = System.Drawing.Color.Silver;
+            this.lblVersion.Location = new System.Drawing.Point(377, 13);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(46, 17);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(663, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Patch any PS1/PS2/PS3/PSP ISO to work on Cobra 7.0 CFW, Powered by PSISO_Tool by " +
+    "CaptainCPS-X";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PSP",
+            "PS3",
+            "PS2",
+            "PS1"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(142, 128);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(508, 22);
+            this.txtPath.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "ISO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Blue;
+            this.textBox1.Location = new System.Drawing.Point(13, 157);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(691, 392);
+            this.textBox1.TabIndex = 8;
+            // 
+            // ps3_ISO_Tool_GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(716, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ps3_ISO_Tool_GUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PS3 ISO Tool GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ PS3 ISO Tool GUI/PS3 ISO Tools GUI/ps3_ISO_Tool_GUI.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace PS3_ISO_Tools_GUI
+{
+    public partial class ps3_ISO_Tool_GUI : Form
+    {
+        public ps3_ISO_Tool_GUI()
+        {
+            InitializeComponent();
+        }
+
+        #region <<< Var's >>>
+
+        string version = "1.03";
+
+        #endregion <<< Var's >>>
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = version;
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog thedialog = new OpenFileDialog();
+            thedialog.Title = "Select ISO";
+            thedialog.Filter = "Image File|*.iso";
+            thedialog.InitialDirectory = Environment.SpecialFolder.MyComputer.ToString();
+            if (thedialog.ShowDialog() == DialogResult.OK)
+            {
+                string isopath = thedialog.FileName;
+                txtPath.Text = isopath;
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName =Application.StartupPath +  @"\\psiso_tool.exe";
+                start.Arguments = "--" + comboBox1.Text.ToLower() + " --verbose " + "\"" + isopath + "\"";
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                start.CreateNoWindow = true;
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        textBox1.Text = result;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog thedialog = new FolderBrowserDialog();
+            if (thedialog.ShowDialog() == DialogResult.OK)
+            {
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    string isopath = thedialog.SelectedPath;
+                    txtPath.Text = isopath;
+                    ProcessStartInfo start = new ProcessStartInfo();
+                    start.FileName = Application.StartupPath + @"\\psiso_tool.exe";
+                    start.Arguments = "--mkps3iso " + "\"" + isopath + "\"";
+                    start.UseShellExecute = false;
+                    start.RedirectStandardOutput = true;
+                    start.CreateNoWindow = true;
+                    using (Process process = Process.Start(start))
+                    {
+                        using (StreamReader reader = process.StandardOutput)
+                        {
+                            string result = reader.ReadToEnd();
+                            textBox1.Text = result;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         
+        }
+    }
+}
