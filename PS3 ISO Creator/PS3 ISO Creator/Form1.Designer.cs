69fbf21e1de8725262cc9f22cbcef69cec819ea0
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2015-06-18 07:31:20 +0200

    PS3 ISO Creator
    Create ISO's from ripped games

---
+++ PS3 ISO Creator/PS3 ISO Creator/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace PS3_ISO_Creator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxVolumeName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(48, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(123, 234);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(527, 22);
+            this.txtPath.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(12, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Create an ISO compatible with ps3";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.ForeColor = System.Drawing.Color.Silver;
+            this.lblVersion.Location = new System.Drawing.Point(347, 22);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(46, 17);
+            this.lblVersion.TabIndex = 11;
+            this.lblVersion.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(155, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 32);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ISO Creator";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PS3_ISO_Creator.Properties.Resources.playstation_3_logo_pngmadden_nfl_25_cheats__trytoolzcom___cheats_bot_trainer_cheat_zwd9zhkq;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Volume Name :";
+            // 
+            // textBoxVolumeName
+            // 
+            this.textBoxVolumeName.Location = new System.Drawing.Point(123, 170);
+            this.textBoxVolumeName.Name = "textBoxVolumeName";
+            this.textBoxVolumeName.Size = new System.Drawing.Size(527, 22);
+            this.textBoxVolumeName.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Folder Path:";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(629, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Abort";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelStatus);
+            this.groupBox2.Controls.Add(this.progressBar);
+            this.groupBox2.Location = new System.Drawing.Point(11, 281);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(693, 73);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Progress";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelStatus.ForeColor = System.Drawing.Color.DarkRed;
+            this.labelStatus.Location = new System.Drawing.Point(558, 19);
+            this.labelStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.labelStatus.Size = new System.Drawing.Size(131, 17);
+            this.labelStatus.TabIndex = 15;
+            this.labelStatus.Text = "Process not started";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 39);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(673, 22);
+            this.progressBar.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(716, 394);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxVolumeName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PS3 ISO Creator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxVolumeName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ PS3 ISO Creator/PS3 ISO Creator/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+using IsoCreator;
+using BER.CDCat.Export;
+
+namespace PS3_ISO_Creator
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_creator = new IsoCreator.IsoCreator();
+            m_creator.Progress += new ProgressDelegate(creator_Progress);
+            m_creator.Finish += new FinishDelegate(creator_Finished);
+            m_creator.Abort += new AbortDelegate(creator_Abort);
+
+        }
+
+        string version = "1.0";
+
+        private Thread m_thread = null;
+        private IsoCreator.IsoCreator m_creator = null;
+
+        private delegate void SetLabelDelegate(string text);
+        private delegate void SetNumericValueDelegate(int value);
+
+        void creator_Abort(object sender, AbortEventArgs e)
+        {
+            MessageBox.Show(e.Message, "Abort", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            MessageBox.Show("The ISO creating process has been stopped.", "Abort", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            button2.Invoke(new Action(() => button2.Enabled = false)); 
+            progressBar.Value = 0;
+            progressBar.Maximum = 0;
+            labelStatus.Text = "Process not started";
+        }
+
+        void creator_Finished(object sender, FinishEventArgs e)
+        {
+            MessageBox.Show(e.Message, "Finish", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            button2.Invoke(new Action(() => button2.Enabled = false)); 
+            progressBar.Value = 0;
+            labelStatus.Text = "Process not started";
+        }
+
+        private void SetLabelStatus(string text)
+        {
+            this.labelStatus.Text = text;
+            this.labelStatus.Refresh();
+        }
+
+        private void SetProgressValue(int value)
+        {
+            this.progressBar.Value = value;
+        }
+
+        private void SetProgressMaximum(int maximum)
+        {
+            this.progressBar.Maximum = maximum;
+        }
+
+        void creator_Progress(object sender, ProgressEventArgs e)
+        {
+            if (e.Action != null)
+            {
+                if (!this.InvokeRequired)
+                {
+                    this.SetLabelStatus(e.Action);
+                }
+                else
+                {
+                    this.Invoke(new SetLabelDelegate(SetLabelStatus), e.Action);
+                }
+            }
+
+            if (e.Maximum != -1)
+            {
+                if (!this.InvokeRequired)
+                {
+                    this.SetProgressMaximum(e.Maximum);
+                }
+                else
+                {
+                    this.Invoke(new SetNumericValueDelegate(SetProgressMaximum), e.Maximum);
+                }
+            }
+
+            if (!this.InvokeRequired)
+            {
+                progressBar.Value = (e.Current <= progressBar.Maximum) ? e.Current : progressBar.Maximum;
+            }
+            else
+            {
+                int value = (e.Current <= progressBar.Maximum) ? e.Current : progressBar.Maximum;
+                this.Invoke(new SetNumericValueDelegate(SetProgressValue), value);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                txtPath.Text = dialog.SelectedPath;
+                textBoxVolumeName.Text = new DirectoryInfo(dialog.SelectedPath).Name + ".iso";
+            }
+
+            DialogResult result = MessageBox.Show("Do you want to create an ISO of this folder for ps3 ?", "PS3ISO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                if (m_thread == null || !m_thread.IsAlive)
+                {
+                    if (textBoxVolumeName.Text.Trim() != "")
+                    {
+                        m_thread = new Thread(new ParameterizedThreadStart(m_creator.Folder2Iso));
+                        m_thread.Start(new IsoCreator.IsoCreator.IsoCreatorFolderArgs(txtPath.Text, Application.StartupPath + "\\" + textBoxVolumeName.Text, textBoxVolumeName.Text));
+
+                        button2.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please insert a name for the volume", "No volume name", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                    }
+                }
+                else
+                {
+                    if (MessageBox.Show("Are you sure you want to abort the process?", "Abort", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        m_thread.Abort();
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (m_creator != null && m_thread != null && m_thread.IsAlive)
+            {
+                m_thread.Abort();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = version;
+        }
+    }
+}
